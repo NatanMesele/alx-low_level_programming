6702cec25977095faa41221fd2526686d5fb3c75
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-15 23:19:47 -1200

    hello

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_abs - givies absolute value
+ *@num : number tobe eval
+ *Return: + ve integer
+ */
+int _abs(int num)
+{
+return ((num >= 0) ? num : num * -1);
+}
+© 2022 GitHub, Inc.
