8387ffafb3cd5ba2f44572fde28ea39dbb1d7d4e
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-15 23:41:34 -1200

    hello

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two ineegers
+ * @n1:first number
+ * @n2: second number
+ * Return: Sums of the numbers
+*/
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}
