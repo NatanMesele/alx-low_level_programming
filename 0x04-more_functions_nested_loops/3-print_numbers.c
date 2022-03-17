524bb70bcb05e81baed482bf305c9c4129989fca
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-17 03:24:39 -1200

    hello

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main"
+
+/**
+* print_numbers - entry point
+*
+* Description: prints the number with _putchar
+*
+* Return: void
+*/
+
+void print_number(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9': number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifnder INV_MAIN_M
+#define INV_MAIN_M
+
+int _putchar(char c);
+
+int _isuper(int c);
+
+int _
