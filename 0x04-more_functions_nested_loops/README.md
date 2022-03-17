9d3b71956988ae2e9fb30a9588251d0b1b99edd4
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-17 06:29:08 -1200

    LEO

--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-My 0x04-more_function_nested_loops task
+My 0x04-more_functions_loops_task 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,11 @@
-int _intsupper(int c);
+int _isupper(int c);
 int _isdigit(int c);
-[A[A[C[C[C[C[C[C[C[[[[C[C[C[C[C[C[C[C[C[supper(int c0;[B);[B
 int mul(int a, int b);
-void print _numbers(void);
+void print_number(void);
 void print_most_numbers(void);
-void more_numbers(void);
+void more_numbers(void)
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[B[C[S[C[C[D[s[C[C[C[C[C[C[B[B[C[C[C[Cs(void);[B[B[B[B[B[B
