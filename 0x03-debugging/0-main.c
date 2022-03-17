e013fcb59b76b2889771efa5034b61f330e9cdd8
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-16 21:20:29 -1200

    go go

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,15 @@
-   #inclide *main.h*
+#include "main.h"
 
-   /**
-   * main - tests function that prints if integer is positive or negative
-   * Return: 0
-   */
+/**
+  * main - Test function for positive or negative
+  * Return: 0
+  */
 
-   int main{void}
-   {
-   int i;
-   i = 0;
+int main(void)
+{
+	int i;
 
-   positive_or_negative(i);
-
-   return (0);
-   }
-
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
