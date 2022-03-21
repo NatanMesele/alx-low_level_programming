72bd8b985486143bb952ae0d874ac0f252e9bb87
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-21 06:38:34 -1200

    hello

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen- returns length of string
+ * @s: string to be counted
+ * Return: int number of s
+ */
+int _strlen(char *s)
+{
+int c;
+
+c = 0;
+while (*s != '\0')
+{
+c++;
+s++;
+}
+return (c);
