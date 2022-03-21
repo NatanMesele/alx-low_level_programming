0a220f7eba8778be688b0e06d2d12517a872c24c
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-21 06:51:47 -1200

    hello

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string- reverses a string
+ * @s: string to be reversed
+ * Return: void
+ */
+void rev_string(char *s)
+{
+char *t = s;
+	char n[1000];
+	short c = 0;
+
+	while (*s != '\0')
+	{
+		n[c] = *s;
+		s++;
+		c++;
+	}
+	c = 0;
+
+	while (s > t)
+	{
+		s--;
+		*s = n[c];
+		c++;
+	}
+}
