209eea39a05778ee4158ed61a4bdf13459442379
Nattyleoalpha <natileoalpha@gmail.com>
2022-03-30 08:17:46 -1200

    go home

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,54 +1,62 @@
 #include "main.h"
 
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
 /**
- * is_palindrome - returns the 1 if s is a palindrome
- * @s: string to be checked
+ * find_strlen - Returns the length of a string.
+ * @s: The string to be measured.
  *
- * Return: 1 if s is a palindrome, 0 otherwise
+ * Return: The length of the string.
+ */
+int find_strlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
+
+/**
+ * check_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ * @len: The length of s.
+ * @index: The index of the string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
+ */
+int check_palindrome(char *s, int len, int index)
+{
+	if (s[index] == s[len / 2])
+		return (1);
+
+	if (s[index] == s[len - index - 1])
+		return (check_palindrome(s, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - Checks if a string is a palindrome.
+ * @s: The string to be checked.
+ *
+ * Return: If the string is a palindrome - 1.
+ *         If the string is not a palindrome - 0.
  */
 int is_palindrome(char *s)
 {
-	int flag = 1;
+	int index = 0;
+	int len = find_strlen(s);
 
-	check(s, 0, _strlen_recursion(s) - 1, &flag);
-	return (flag);
-}
+	if (!(*s))
+		return (1);
 
-/**
- * check - checks if a string is a palindrome
- * @s: string to be checked
- * @start: start index
- * @end: end index
- * @flag: flag to indicate if a string is a palindrome
- *
- * Return: void
- */
-void check(char *s, int start, int end, int *flag)
-{
-	if (start <= end)
-	{
-		if (s[start] == s[end])
-			*flag *= 1;
-		else
-			*flag *= 0;
-		check(s, start + 1, end - 1, flag);
-	}
-}
-
-/**
- * _strlen_recursion - calculates the length of a string
- * @s: string to be used
- *
- * Return: length of the string
- */
-int _strlen_recursion(char *s)
-{
-	int sum = 0;
-
-	if (*s != '\0')
-	{
-		sum++;
-		sum += _strlen_recursion(s + 1);
-	}
-	return (sum);
+	return (check_palindrome(s, len, index));
 }
--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,19 +1,30 @@
 #include "main.h"
-
 /**
- * wildcmp - compares two strings and returns 1 if identical
- * @s1: string to be checked
- * @s2: pattern to be used
- *
- * Return: 1 if identical, 0 otherwise
+ * wildcmp - compare two strings with possible wildcards
+ * @s1: string
+ * @s2: string
+ * Return:int 0 or 1
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s2 == '\0')
-		return (*s1 == '\0');
-	if (*s2 == *s1)
-		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	else if (*s1 == '\0' && *s2 == '*')
+		return (wildcmp(s1, (s2 + 1)));
+	else if (*s1 == '\0' || *s2 == '\0')
+		return (0);
+
+	if (*s1 == *s2)
+		return (wildcmp((s1 + 1), (s2 + 1)));
+
 	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, (s2 + 1)));
+		else if (wildcmp(s1, s2 + 1))
+			return (1);
+		else
+			return (wildcmp(s1 + 1, s2));
+	}
 	return (0);
 }
