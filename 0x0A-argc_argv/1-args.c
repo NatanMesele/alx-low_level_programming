cd66cf1e0e76043ddc600b14fbee5beed84ba426
Nattyleoalpha <natileoalpha@gmail.com>
2022-04-01 09:09:31 -1200

    go home

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Display the number of arguments passed to it.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0 (Success)
- */
-int main(int argc, char *argv[] __attribute__((unused)))
+* main - prints the number of arguments
+*@argc: number of arguments
+*@argv:array of arguments
+*Return: the number of arguments
+*/
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
+	printf("%d\n", argc - 1);
 
-		printf("%d\n", argc - 1);
-
-	ret
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_M_INCLUDED
+#define MAIN_M_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+
+#endif
