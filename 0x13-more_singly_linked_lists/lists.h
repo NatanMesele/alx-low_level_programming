eb4a3b72e4c48f4ef86fc656bea60b97b599157c
Natan Mesele <99384070+Nattyleoalpha@users.noreply.github.com>
2022-04-26 08:49:11 -1200

    you can make it

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,20 +1,19 @@
-#ifndef LISTS_H_
-#define LISTS_H_
-
+#ifndef LISTS_H
+#define LISTS_H
 #include <stdlib.h>
-#include <string.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -32,4 +31,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif
